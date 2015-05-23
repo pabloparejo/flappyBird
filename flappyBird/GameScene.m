1cdee28542d8bd84dd7ae30b9edc88cb78155f4c
pabloparejo <parejo.p@gmail.com>
2015-05-23 15:00:42 +0200

    Game reset

--- flappyBird/GameScene.m
+++ flappyBird/GameScene.m
@@ -10,11 +10,18 @@
 
 static UInt32 birdGroup = 1 << 0;   // 0001
 static UInt32 objectGroup = 1 << 1; // 0010
+static UInt32 gapGroup = 1 << 2; // 0010
 
 @interface GameScene() <SKPhysicsContactDelegate>
+
 @property (strong, nonatomic) SKSpriteNode *bird;
 @property (strong, nonatomic) SKNode *movingObjects;
 @property (strong, nonatomic) SKLabelNode *gameOver;
+@property (strong, nonatomic) SKLabelNode *scoreLabel;
+
+@property (nonatomic) BOOL isAlive;
+@property (nonatomic) int score;
+
 @end
 
 @implementation GameScene
@@ -25,19 +32,35 @@ static UInt32 objectGroup = 1 << 1; // 0010
     self.physicsWorld.contactDelegate = self;
     
     self.movingObjects = [[SKNode alloc] init];
-    [self addChild:self.movingObjects];
+    
+    [self initGame];
+}
 
+-(void) initGame{
+    self.isAlive = YES;
+    self.score = 0;
+    [self removeAllChildren];
+    [self.movingObjects removeAllChildren];
+    [self addChild:self.movingObjects];
     [self createBg];
     [self createFloor];
+    [self createScore];
+    self.movingObjects.speed = 1;
     
     // Start game after 3 seconds
-    [self performSelector:@selector(createBird) withObject:nil afterDelay:1];
-    [self performSelector:@selector(createPipes) withObject:nil afterDelay:3];
+    [self performSelector:@selector(createBird) withObject:nil afterDelay:0.5];
+    [self performSelector:@selector(createPipes) withObject:nil afterDelay:2];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    [super touchesBegan:touches withEvent:event];
-    [self.bird.physicsBody applyImpulse:CGVectorMake(0, 200)];
+    if (self.isAlive) {
+        [super touchesBegan:touches withEvent:event];
+        [self.bird.physicsBody applyImpulse:CGVectorMake(0, 200)];
+    }else{
+        //reset game
+        [self initGame];
+    }
+    
     
 }
 
@@ -47,14 +70,14 @@ static UInt32 objectGroup = 1 << 1; // 0010
 
 -(void) createBird{
     self.bird = [SKSpriteNode spriteNodeWithImageNamed:@"flappy2"];
-    self.bird.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    self.bird.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame)-100);
     self.bird.zPosition = 10;
     
     self.bird.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:self.bird.size.width / 2];
     self.bird.physicsBody.dynamic = YES;
     self.bird.physicsBody.categoryBitMask = birdGroup;
     self.bird.physicsBody.collisionBitMask = objectGroup;
-    self.bird.physicsBody.contactTestBitMask = objectGroup;
+    self.bird.physicsBody.contactTestBitMask = objectGroup | gapGroup;
     
     self.bird.physicsBody.allowsRotation = NO;
     
@@ -112,58 +135,83 @@ static UInt32 objectGroup = 1 << 1; // 0010
 }
 
 -(void) createPipes{
-    
-    static CGFloat gap = 200;
-    int pipeOffset = arc4random_uniform(self.frame.size.height / 2) - self.frame.size.height / 4;
-    
-    int pipeDistance = arc4random_uniform(.5) + 2;
-    
-    SKSpriteNode *pipeUp = [SKSpriteNode spriteNodeWithImageNamed:@"pipe1"];
-    pipeUp.anchorPoint = CGPointMake(0, 0);
-    pipeUp.position = CGPointMake(self.frame.size.width, CGRectGetMidY(self.frame) + gap/2 + pipeOffset);
-    
-    pipeUp.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:pipeUp.size center:CGPointMake(pipeUp.size.width / 2,pipeUp.size.height / 2)];
-    pipeUp.physicsBody.dynamic = NO;
-    pipeUp.physicsBody.categoryBitMask = objectGroup;
-    
+    if (self.isAlive) {
+        static CGFloat gap = 200;
+        int pipeOffset = arc4random_uniform(self.frame.size.height / 2) - self.frame.size.height / 4;
+        
+        int pipeDistance = arc4random_uniform(.5) + 2;
+        
+        SKSpriteNode *pipeUp = [SKSpriteNode spriteNodeWithImageNamed:@"pipe1"];
+        pipeUp.anchorPoint = CGPointMake(0, 0);
+        pipeUp.position = CGPointMake(self.frame.size.width, CGRectGetMidY(self.frame) + gap/2 + pipeOffset);
+        
+        pipeUp.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:pipeUp.size center:CGPointMake(pipeUp.size.width / 2,pipeUp.size.height / 2)];
+        pipeUp.physicsBody.dynamic = NO;
+        pipeUp.physicsBody.categoryBitMask = objectGroup;
+        
+        
+        SKSpriteNode *pipeDown = [SKSpriteNode spriteNodeWithImageNamed:@"pipe2"];
+        pipeDown.anchorPoint = CGPointMake(0, 1);
+        pipeDown.position = CGPointMake(self.frame.size.width, CGRectGetMidY(self.frame) - gap/2 + pipeOffset);
+        
+        pipeDown.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:pipeDown.size center:CGPointMake(pipeDown.size.width / 2, -pipeDown.size.height / 2)];
+        pipeDown.physicsBody.dynamic = NO;
+        pipeDown.physicsBody.categoryBitMask = objectGroup;
+        
+        SKSpriteNode *gapNode = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(pipeUp.size.width, gap)];
+        gapNode.anchorPoint = CGPointMake(0,1);
+        gapNode.position = pipeUp.position;
+        gapNode.zPosition = 100;
+        
+        gapNode.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:gapNode.size center:CGPointMake(gapNode.size.width / 2, -gapNode.size.height/2)];
+        gapNode.physicsBody.dynamic = NO;
+        gapNode.physicsBody.categoryBitMask = gapGroup;
+        
+        SKAction *roll = [SKAction moveToX:-pipeUp.size.width duration:3];
+        SKAction *remove = [SKAction removeFromParent];
+        
+        SKAction *pipeActions = [SKAction sequence:@[roll, remove]];
+        
+        [pipeUp runAction:pipeActions];
+        [pipeDown runAction:pipeActions];
+        [gapNode runAction:pipeActions];
+        
+        
+        [self.movingObjects addChild:pipeUp];
+        [self.movingObjects addChild:pipeDown];
+        [self.movingObjects addChild:gapNode];
+        
+        [self performSelector:@selector(createPipes) withObject:nil afterDelay:pipeDistance];
+    }
+}
 
-    SKSpriteNode *pipeDown = [SKSpriteNode spriteNodeWithImageNamed:@"pipe2"];
-    pipeDown.anchorPoint = CGPointMake(0, 1);
-    pipeDown.position = CGPointMake(self.frame.size.width, CGRectGetMidY(self.frame) - gap/2 + pipeOffset);
-    
-    pipeDown.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:pipeDown.size center:CGPointMake(pipeDown.size.width / 2, -pipeDown.size.height / 2)];
-    pipeDown.physicsBody.dynamic = NO;
-    pipeDown.physicsBody.categoryBitMask = objectGroup;
-    
-    SKAction *roll = [SKAction moveToX:-pipeUp.size.width duration:3];
-    SKAction *remove = [SKAction removeFromParent];
-    
-    SKAction *pipeActions = [SKAction sequence:@[roll, remove]];
-    
-    [pipeUp runAction:pipeActions];
-    [pipeDown runAction:pipeActions];
-    
-    
-    
-    [self.movingObjects addChild:pipeUp];
-    [self.movingObjects addChild:pipeDown];
-
-    [self performSelector:@selector(createPipes) withObject:nil afterDelay:pipeDistance];
+-(void) createScore{
+    self.scoreLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    self.scoreLabel.zPosition = 100;
+    self.scoreLabel.fontColor = [UIColor whiteColor];
+    self.scoreLabel.fontSize = 60.0f;
+    self.scoreLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMaxY(self.frame) - 60);
+    self.scoreLabel.text = @"0";
+    [self addChild:self.scoreLabel];
 }
 
 #pragma mark - SKPhysicsContactDelegate
 
 -(void) didBeginContact:(SKPhysicsContact *)contact{
-    NSLog(@"WHATSUP");
-    self.movingObjects.speed = 0;
-    
-    self.gameOver = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    self.gameOver.zPosition = 100;
-    self.gameOver.fontColor = [UIColor whiteColor];
-    self.gameOver.fontSize = 30.0f;
-    self.gameOver.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
-    self.gameOver.text = @"Game Over";
-    [self addChild:self.gameOver];
+    if ((contact.bodyA.categoryBitMask == gapGroup || contact.bodyB.categoryBitMask == gapGroup) && self.isAlive) {
+        self.score += 1;
+        self.scoreLabel.text = [NSString stringWithFormat:@"%d", self.score];
+    }else{
+        self.isAlive = NO;
+        self.movingObjects.speed = 0;
+        self.gameOver = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        self.gameOver.zPosition = 100;
+        self.gameOver.fontColor = [UIColor whiteColor];
+        self.gameOver.fontSize = 40.0f;
+        self.gameOver.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+        self.gameOver.text = @"Game Over";
+        [self addChild:self.gameOver];
+    }
 }
 
 @end
