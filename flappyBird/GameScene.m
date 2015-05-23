b60c26137971173b38ca2f2ca89004dc564aaf98
pabloparejo <parejo.p@gmail.com>
2015-05-23 11:26:50 +0200

    bg image as node

--- flappyBird/GameScene.m
+++ flappyBird/GameScene.m
@@ -11,7 +11,14 @@
 @implementation GameScene
 
 -(void)didMoveToView:(SKView *)view {
+    SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"bg"];
+    background.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    background.size = self.frame.size;
+    [self addChild:background];
     
+    SKSpriteNode *bird = [SKSpriteNode spriteNodeWithImageNamed:@"flappy1"];
+    bird.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    [self addChild:bird];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
